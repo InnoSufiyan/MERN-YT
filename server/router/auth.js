04ac65d3c6002fc6a97e7417dc218db36c1db2e6
inno sufiyan <innosufiyan@gmail.com>
2022-04-10 18:14:25 +0500

    class 9

--- server/router/auth.js
+++ server/router/auth.js
@@ -2,13 +2,45 @@ const express = require('express')
 
 const router = express.Router()
 
+require('../db/conn')
+
+const User = require("../model/userSchema")
+
 router.get('/', (req, res)=> {
     res.send('Hello from the Router Server')
 })
 
 router.post('/register', (req, res)=> {
-    console.log(req.body)
-    res.send(req.body)
+    const {name, email, phone, work, password, cpassword} = req.body
+    
+    if(!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({
+            error: "Please fill the field properly"
+        })
+    }
+
+    User.findOne({
+        email: email
+    }).then((userExist)=> {
+        if(userExist) {
+            return res.status(422).json({
+                error: "Email already exist"
+            })
+        }
+
+        const user = new User({
+            name, email, phone, work, password, cpassword
+        })
+
+        user.save().then(()=> {
+            res.status(201).json({
+                message: "User saved"
+            })
+        }).catch((err)=> res.status(500).json({
+            error: "Failed to register"
+        }))
+
+    }).catch(err => {console.log(err)})
 })
 
 module.exports = router;
